4800fa8004989aa26c047f613d91fadcfcbd4995
Joanna May <joannamadisonmay@gmail.com>
2023-04-08 16:35:45 -0500

    fix: don't compile preprocessor defines

--- src/Main.cs
+++ src/Main.cs
@@ -2,10 +2,12 @@ namespace Chickensoft.GodotGame;
 
 using Godot;
 
+//-:cnd:noEmit
 #if DEBUG
 using System.Reflection;
 using Chickensoft.GoDotTest;
 #endif
+//+:cnd:noEmit
 
 // This entry-point file is responsible for determining if we should run tests.
 //
@@ -16,6 +18,7 @@ public partial class Main : Node2D {
   public TestEnvironment Environment = default!;
 
   public override void _Ready() {
+//-:cnd:noEmit
 #if DEBUG
     // If this is a debug build, use GoDotTest to examine the
     // command line arguments and determine if we should run tests.
@@ -25,6 +28,8 @@ public partial class Main : Node2D {
       return;
     }
 #endif
+//+:cnd:noEmit
+
     // If we don't need to run tests, we can just switch to the game scene.
     GetTree().ChangeSceneToFile("res://src/Game.tscn");
   }
