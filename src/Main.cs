610a9ba7c79c8f123b08bf6af7607bebc5166be9
Joanna May <joannamadisonmay@gmail.com>
2023-10-04 17:09:09 -0500

    fix: compilation being removed

--- src/Main.cs
+++ src/Main.cs
@@ -15,12 +15,14 @@ using Chickensoft.GoDotTest;
 // Game.cs instead.
 
 public partial class Main : Node2D {
+  //-:cnd:noEmit
 #if DEBUG
   public TestEnvironment Environment = default!;
 #endif
+  //+:cnd:noEmit
 
   public override void _Ready() {
-//-:cnd:noEmit
+    //-:cnd:noEmit
 #if DEBUG
     // If this is a debug build, use GoDotTest to examine the
     // command line arguments and determine if we should run tests.
@@ -30,14 +32,16 @@ public partial class Main : Node2D {
       return;
     }
 #endif
-//+:cnd:noEmit
+    //+:cnd:noEmit
 
     // If we don't need to run tests, we can just switch to the game scene.
     GetTree().ChangeSceneToFile("res://src/Game.tscn");
   }
 
+  //-:cnd:noEmit
 #if DEBUG
   private void RunTests()
     => _ = GoTest.RunTests(Assembly.GetExecutingAssembly(), this, Environment);
 #endif
+  //+:cnd:noEmit
 }
